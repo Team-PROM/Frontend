cf8191275a2fbf99679ab1e2d478656fad18f609
dutexion <dutexion@gmail.com>
2023-10-16 20:13:39 +0900

    세팅

--- prom/src/App.js
+++ prom/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { styled } from "styled-components";
 
-function App() {
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+export default function App() {
+  console.log(width, height);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body width={width} height={height}>
+      a
+    </Body>
   );
 }
 
-export default App;
+const Body = styled.div`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+`;
