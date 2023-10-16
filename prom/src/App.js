d35f344aa187c7607e18c78cb26f0b0780379570
dutexion <dutexion@gmail.com>
2023-10-16 22:37:36 +0900

    음... 레이아웃 완성?

--- prom/src/App.js
+++ prom/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { styled } from "styled-components";
+import { theme } from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
+import Header from "./components/header";
+import Menu from "./components/menu";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -7,13 +11,31 @@ const height = window.innerHeight;
 export default function App() {
   console.log(width, height);
   return (
-    <Body width={width} height={height}>
-      a
-    </Body>
+    <Wrapper>
+      <GlobalStyle />
+      <Menu></Menu>
+      <SmallWrapper>
+        <Header />
+        <Body width={width} height={height} theme={theme}>
+          body
+        </Body>
+      </SmallWrapper>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const SmallWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100% - 218px);
+`;
+
 const Body = styled.div`
-  width: ${({ width }) => `${width}px`};
+  width: 100%;
   height: ${({ height }) => `${height}px`};
+  background-color: ${({ theme }) => `${theme.background}`};
 `;
