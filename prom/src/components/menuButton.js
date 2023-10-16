6aa3f679b7f91ddf3d60ce64b917828b4904d34a
dutexion <dutexion@gmail.com>
2023-10-16 22:32:56 +0900

    메뉴 버튼

---
+++ prom/src/components/menuButton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { styled } from "styled-components";
+
+export default function MenuButton({ children }) {
+  return <Body>{children}</Body>;
+}
+
+const Body = styled.div`
+  width: 100%;
+  height: 38px;
+  text-align: center;
+  line-height: 38px;
+  font-size: 14px;
+  color: white;
+  transition: 0.2s ease-in-out;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+`;
