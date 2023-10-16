38c28c3454d3bddefedc68b4aefd4a1d91ee1bea
dutexion <dutexion@gmail.com>
2023-10-16 22:37:11 +0900

    theme

---
+++ prom/src/styles/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  background: "#1C1B23",
+  menuback: "#252631",
+
+  purple1: "#B11FF5",
+  purple2: "#B948EE",
+  purple3: "#C583F9",
+
+  sub1: "#64768C",
+  sub2: "#B1B8C0",
+  sub3: "#D6DADF",
+
+  text1: "#1A1E27",
+  text2: "#505866",
+  text3: "#D6DADF",
+
+  white: "#FFFFFF",
+  gray1: "#2E2E2E",
+  gray2: "#414347",
+};
