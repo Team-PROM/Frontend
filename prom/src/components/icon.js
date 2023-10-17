daf0754c5603fae435af64fcc4f9edc69b98f90d
dutexion <dutexion@gmail.com>
2023-10-17 22:02:58 +0900

    _Icon 수정

--- prom/src/components/icon.js
+++ prom/src/components/icon.js
@@ -6,19 +6,23 @@ import { ReactComponent as List } from "../asset/gradientIcon/calender.svg";
 import { ReactComponent as Project } from "../asset/gradientIcon/calender.svg";
 import { ReactComponent as Question } from "../asset/gradientIcon/calender.svg";
 import { ReactComponent as Team } from "../asset/gradientIcon/calender.svg";
+import { styled } from "styled-components";
 
 export default function _Icon({ select, icon }) {
   if (select) {
     switch (icon) {
       case "alarm":
         return (
-          <Alarm
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <Select>
+            <Alarm
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />{" "}
+            알림
+          </Select>
         );
       case "calender":
         return (
@@ -85,81 +89,122 @@ export default function _Icon({ select, icon }) {
     switch (icon) {
       case "alarm":
         return (
-          <Icon
-            icon='clarity:bell-line'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='clarity:bell-line'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />{" "}
+            알림
+          </NoSelect>
         );
       case "calender":
         return (
-          <Icon
-            icon='system-uicons:calendar'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='system-uicons:calendar'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />
+            캘린더
+          </NoSelect>
         );
       case "line":
         return (
-          <Icon
-            icon='ic:baseline-insights'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='ic:baseline-insights'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />
+            분석
+          </NoSelect>
         );
       case "list":
         return (
-          <Icon
-            icon='fluent:task-list-20-filled'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='fluent:task-list-20-filled'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />
+            작업
+          </NoSelect>
         );
       case "project":
         return (
-          <Icon
-            icon='ion:rocket-outline'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='ion:rocket-outline'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />
+            프로젝트
+          </NoSelect>
         );
       case "question":
         return (
-          <Icon
-            icon='solar:question-circle-linear'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='solar:question-circle-linear'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />
+            이용 가이드
+          </NoSelect>
         );
       case "team":
         return (
-          <Icon
-            icon='ion:people-outline'
-            style={{
-              marginLeft: "30px",
-              marginRight: "15px",
-              fontSize: "20px",
-            }}
-          />
+          <NoSelect>
+            <Icon
+              icon='ion:people-outline'
+              style={{
+                marginLeft: "30px",
+                marginRight: "15px",
+                fontSize: "20px",
+              }}
+            />
+            팀원
+          </NoSelect>
         );
     }
   }
 }
+
+const Select = styled.div`
+  font-size: 14px;
+  background: var(
+    --purple-gradation,
+    linear-gradient(180deg, #ee9ae5 0%, #5961f9 100%)
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  display: flex;
+  align-items: center;
+`;
+
+const NoSelect = styled.div`
+  font-size: 14px;
+  color: white;
+  display: flex;
+  align-items: center;
+`;
