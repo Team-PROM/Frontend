1babbe6f6ca91e696554bcf024f070144ad77f81
dutexion <dutexion@gmail.com>
2023-10-17 22:57:49 +0900

    페이지들 기본 틀

---
+++ prom/src/pages/team.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { styled } from "styled-components";
+import { theme } from "../styles/theme";
+
+export default function TeamPage() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const setSize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", setSize);
+    return () => {
+      window.removeEventListener("resize", setSize);
+    };
+  }, []);
+
+  return (
+    <Body height={height} theme={theme}>
+      AlarmPage
+    </Body>
+  );
+}
+
+const Body = styled.div`
+  width: 100%;
+  height: ${({ height }) => `${height}px`};
+  background-color: ${({ theme }) => `${theme.background}`};
+`;
