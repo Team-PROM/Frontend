3d1d7b5a6492a1e3f48a26e0b741e0669a977204
dutexion <dutexion@gmail.com>
2023-10-17 14:02:48 +0900

    메뉴에 아이콘 추가

--- prom/src/components/menu.js
+++ prom/src/components/menu.js
@@ -2,6 +2,7 @@ import React from "react";
 import { styled } from "styled-components";
 import { theme } from "../styles/theme";
 import MenuButton from "./menuButton";
+import { Icon } from "@iconify/react";
 
 export default function Menu() {
   return (
@@ -11,16 +12,99 @@ export default function Menu() {
           <TitleLogo></TitleLogo>
           <TitleText theme={theme}>APPLE</TitleText>
         </TitleContainer>
-        <TitleButton></TitleButton>
+        <TitleButton>
+          <Icon
+            icon='bxs:up-arrow'
+            style={{
+              fontSize: "10px",
+            }}
+          />
+          <Icon
+            icon='bxs:down-arrow'
+            style={{
+              fontSize: "10px",
+            }}
+          />
+        </TitleButton>
       </Project>
       <ButtonContainer>
-        <MenuButton>알림</MenuButton>
-        <MenuButton>프로젝트</MenuButton>
-        <MenuButton>작업</MenuButton>
-        <MenuButton>캘린더</MenuButton>
-        <MenuButton>분석</MenuButton>
-        <MenuButton>팀원</MenuButton>
-        <MenuButton>이용약관</MenuButton>
+        <MenuButton>
+          <Icon
+            icon='clarity:bell-line'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          알림
+        </MenuButton>
+        <MenuButton>
+          <Icon
+            icon='ion:rocket-outline'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          프로젝트
+        </MenuButton>
+        <MenuButton>
+          <Icon
+            icon='fluent:task-list-20-filled'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          작업
+        </MenuButton>
+        <MenuButton>
+          <Icon
+            icon='system-uicons:calendar'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          캘린더
+        </MenuButton>
+        <MenuButton>
+          <Icon
+            icon='ic:baseline-insights'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          분석
+        </MenuButton>
+        <MenuButton>
+          <Icon
+            icon='ion:people-outline'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          팀원
+        </MenuButton>
+        <MenuButton>
+          <Icon
+            icon='solar:question-circle-linear'
+            style={{
+              marginLeft: "30px",
+              marginRight: "15px",
+              fontSize: "20px",
+            }}
+          />
+          이용약관
+        </MenuButton>
       </ButtonContainer>
     </Body>
   );
@@ -65,7 +149,8 @@ const TitleText = styled.div`
 const TitleButton = styled.div`
   width: 10px;
   height: 20px;
-  background-color: white;
+  display: flex;
+  flex-direction: column;
 `;
 
 const ButtonContainer = styled.div`
