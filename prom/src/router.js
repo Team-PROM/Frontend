3261a5bf6dd29fe65c001672d2d3818daf0e4b3f
dutexion <dutexion@gmail.com>
2023-10-17 23:04:33 +0900

    라우터 설정

--- prom/src/router.js
+++ prom/src/router.js
@@ -1,12 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import ProjectPage from "./pages/project";
 import AlarmPage from "./pages/alarm";
+import AnalysisPage from "./pages/analysis";
+import CalenderPage from "./pages/calender";
+import GuidePage from "./pages/guide";
+import TaskPage from "./pages/task";
+import TeamPage from "./pages/team";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<ProjectPage></ProjectPage>} />
       <Route path='/alarm' element={<AlarmPage></AlarmPage>} />
+      <Route path='/analysis' element={<AnalysisPage></AnalysisPage>} />
+      <Route path='/calender' element={<CalenderPage></CalenderPage>} />
+      <Route path='/guide' element={<GuidePage></GuidePage>} />
+      <Route path='/task' element={<TaskPage></TaskPage>} />
+      <Route path='/team' element={<TeamPage></TeamPage>} />
     </Routes>
   );
 };
