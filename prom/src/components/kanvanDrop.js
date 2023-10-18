ebfbb75c47587695b4fe13b109057502caac8bba
dutexion <dutexion@gmail.com>
2023-10-18 22:52:32 +0900

    이미지 렌더

--- prom/src/components/kanvanDrop.js
+++ prom/src/components/kanvanDrop.js
@@ -1,29 +1,39 @@
 import { Droppable } from "react-beautiful-dnd";
 import Item from "./kanvanDrag";
 import { styled } from "styled-components";
+import { ReactComponent as TodoImg } from "../asset/projectPageImg/todo.svg";
+import { ReactComponent as CompleteImg } from "../asset/projectPageImg/complete.svg";
+import { ReactComponent as ProgressImg } from "../asset/projectPageImg/progress.svg";
 
 export default function kanvanDrop({ columnId, column }) {
+  const Img = () => {
+    if (column.title == "할 일") {
+      if (column.title == "진행 중") {
+        console.log(123);
+        if (column.title == "완료") {
+          return <CompleteImg />;
+        }
+        return <ProgressImg />;
+      }
+      return <TodoImg />;
+    }
+  };
+
   return (
     <Wrapper key={columnId}>
-      <h2>{column.title}</h2>
+      <Title>
+        {Img()}
+        {column.title}
+      </Title>
       <Droppable droppableId={columnId} key={columnId}>
         {(provided, snapshot) => {
           return (
-            <div
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              style={{
-                background: snapshot.isDraggingOver ? "lightblue" : "lightgrey",
-                padding: 4,
-                width: 250,
-                margin: 8,
-                minHeight: 500,
-              }}>
+            <Container {...provided.droppableProps} ref={provided.innerRef}>
               {column.items.map((item, index) => {
                 return <Item item={item} index={index} />;
               })}
               {provided.placeholder}
-            </div>
+            </Container>
           );
         }}
       </Droppable>
@@ -35,4 +45,22 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 280px;
+  height: 600px;
+`;
+
+const Title = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 36px;
+  display: flex;
+  width: 280px;
+  > :first-child {
+    margin-right: 10px;
+  }
+`;
+
+const Container = styled.div`
+  width: 280px;
+  height: 600px;
 `;
