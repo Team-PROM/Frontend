2e71a08b471d74cbc7a5a599901a92d22e708f8d
dutexion <dutexion@gmail.com>
2023-10-19 22:54:49 +0900

    쓸모 없는 코드 정리

--- prom/src/components/kanvanDrop.js
+++ prom/src/components/kanvanDrop.js
@@ -1,30 +1,10 @@
 import { Droppable } from "react-beautiful-dnd";
 import Item from "./kanvanDrag";
 import { styled } from "styled-components";
-import { ReactComponent as TodoImg } from "../asset/projectPageImg/todo.svg";
-import { ReactComponent as CompleteImg } from "../asset/projectPageImg/complete.svg";
-import { ReactComponent as ProgressImg } from "../asset/projectPageImg/progress.svg";
 
 export default function kanvanDrop({ columnId, column }) {
-  const Img = () => {
-    if (column.title == "할 일") {
-      if (column.title == "진행 중") {
-        console.log(123);
-        if (column.title == "완료") {
-          return <CompleteImg />;
-        }
-        return <ProgressImg />;
-      }
-      return <TodoImg />;
-    }
-  };
-
   return (
     <Wrapper key={columnId}>
-      <Title>
-        {Img()}
-        {column.title}
-      </Title>
       <Droppable droppableId={columnId} key={columnId}>
         {(provided, snapshot) => {
           return (
