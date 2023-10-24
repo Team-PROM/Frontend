4dd1dc7346d40a8158ad5e35942404e33f4f0250
dutexion <dutexion@gmail.com>
2023-10-24 14:49:24 +0900

    드래그 관련 스타일 정의

--- prom/src/styles/GlobalStyle.js
+++ prom/src/styles/GlobalStyle.js
@@ -5,9 +5,18 @@ const GlobalStyle = createGlobalStyle`
     margin:0;
     overflow: hidden;
     font-family:"Pretendard";
+    & ::selection {
+      background-color:rgba(0,0,0,0)
+    }
   }
   a {
     text-decoration:none;
+  }
+  input {
+    -webkit-user-select:all;
+    -moz-user-select:all;
+    -ms-user-select:all;
+    user-select:all;
   }
 `;
 
