c127ff3a0f8281c83fe0161fe8d5203ea7d2ebbc
dutexion <dutexion@gmail.com>
2023-10-24 19:01:01 +0900

    theme 추가

--- prom/src/styles/theme.js
+++ prom/src/styles/theme.js
@@ -1,4 +1,4 @@
-export const theme = {
+const color = {
   background: "#1C1B23",
   menuback: "#252631",
 
@@ -17,4 +17,42 @@ export const theme = {
   white: "#FFFFFF",
   gray1: "#2E2E2E",
   gray2: "#414347",
+};
+
+const size = {
+  bigTitle: "22px",
+  boldTitle: "20px",
+  caption: "16px",
+  content: "18px",
+  sectionTitle: "18px",
+  tag: "16px",
+  sectionTitleMedium: "18px",
+  captionSemiBold: "16px",
+  mini: "14px",
+  miniMedium: "14px",
+  microMedium: "12px",
+  supermicroMedium: "10px",
+  microLight: "12px",
+};
+
+const weight = {
+  bigTitle: "700",
+  boldTitle: "700",
+  caption: "500",
+  content: "500",
+  sectionTitle: "600",
+  tag: "600",
+  sectionTitleMedium: "500",
+  captionSemiBold: "600",
+  mini: "600",
+  miniMedium: "500",
+  microMedium: "500",
+  supermicroMedium: "500",
+  microLight: "300",
+};
+
+export const theme = {
+  color,
+  size,
+  weight,
 };
