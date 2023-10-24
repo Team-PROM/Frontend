9407fcea43bc04b30174bf94ffa705300f834750
dutexion <dutexion@gmail.com>
2023-10-24 22:20:21 +0900

    셀렉 메뉴들 삽입

--- prom/src/pages/project.js
+++ prom/src/pages/project.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { styled } from "styled-components";
 import KanbanBoard from "../components/kanbanBoard";
 import Body from "../components/common/pageBody";
 import SelectMenu from "../components/selectMenu";
+import ToggleButton from "../components/toggleButton";
 
 export default function ProjectPage() {
   return (
     <Body>
-      <SelectMenu />
+      <TopContainer>
+        <SelectMenu keyword='team' />
+        <SelectMenu keyword='domain' />
+        <SelectMenu keyword='tag' />
+        <ToggleButton />
+      </TopContainer>
       <KanbanBoard></KanbanBoard>
     </Body>
   );
 }
+
+const TopContainer = styled.div`
+  margin-left: 30px;
+  gap: 8px;
+  display: flex;
+  align-items: center;
+  height: 30px;
+  margin-bottom: 40px;
+  > :last-child {
+    margin-left: 12px;
+  }
+`;
