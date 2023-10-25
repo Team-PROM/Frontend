48a4d896c1204a3571449b12790af2523a028ae2
dutexion <dutexion@gmail.com>
2023-10-25 23:21:46 +0900

    모달 기본 틀

--- prom/src/components/modal/modal.js
+++ prom/src/components/modal/modal.js
@@ -1,3 +1,11 @@
+import { useRecoilState } from "recoil";
+import { modalState } from "../../utils/atom";
+
 export default function Modal() {
-  return <div></div>;
+  const [useModalState, setModalState] = useRecoilState(modalState);
+
+  switch (useModalState) {
+    case "project":
+      return;
+  }
 }
