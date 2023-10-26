4b1faf98bc7c302aca5e3ad2c5ae9fc166ce0f8a
dutexion <dutexion@gmail.com>
2023-10-26 11:37:09 +0900

    프로젝트 페이지 레이아웃 및 작업 진행도 50% 퍼블리싱

--- prom/src/pages/project.js
+++ prom/src/pages/project.js
@@ -5,18 +5,35 @@ import Body from "../components/common/pageBody";
 import SelectMenu from "../components/selectMenu";
 import ToggleButton from "../components/toggleButton";
 import SearchInput from "../components/searchInput";
+import { Icon } from "@iconify/react";
 
 export default function ProjectPage() {
   return (
     <Body>
       <TopContainer>
-        <SearchInput />
-        <SelectContainer>
-          <SelectMenu keyword='team' />
-          <SelectMenu keyword='domain' />
-          <SelectMenu keyword='tag' />
-          <ToggleButton />
-        </SelectContainer>
+        <TopLeftContainer>
+          <SearchInput />
+          <SelectContainer>
+            <SelectMenu keyword='team' />
+            <SelectMenu keyword='domain' />
+            <SelectMenu keyword='tag' />
+            <ToggleButton />
+          </SelectContainer>
+        </TopLeftContainer>
+        <TopRightContainer>
+          <TaskProgress>
+            <ProgressText>
+              <div>
+                작업 진행률
+                <span>(43%)</span>
+              </div>
+              <div>
+                <Icon icon='ci:info' width='18' />
+              </div>
+            </ProgressText>
+            <ProgressBar></ProgressBar>
+          </TaskProgress>
+        </TopRightContainer>
       </TopContainer>
       <KanbanBoard />
     </Body>
@@ -25,10 +42,52 @@ export default function ProjectPage() {
 
 const TopContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
   margin-left: 30px;
+  margin-right: 40px;
+  margin-top: 40px;
+`;
+
+const TopLeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   gap: 30px;
 `;
+
+const TopRightContainer = styled.div``;
+
+const TaskProgress = styled.div`
+  width: 232px;
+  height: 48px;
+  padding: 16px 24px 16px 24px;
+  border-radius: 15px;
+  background-color: ${({ theme }) => theme.color.gray1};
+  border: 1px solid ${({ theme }) => theme.color.text2};
+`;
+
+const ProgressText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+    font-size: ${({ theme }) => theme.size.miniMedium};
+    font-size: ${({ theme }) => theme.weight.miniMedium};
+    color: ${({ theme }) => theme.color.white};
+    span {
+      margin-left: 2px;
+      font-size: ${({ theme }) => theme.size.microMedium};
+      font-size: ${({ theme }) => theme.weight.microMedium};
+      color: ${({ theme }) => theme.color.text3};
+    }
+  }
+  > :last-child {
+    width: 18px;
+    height: 18px;
+  }
+`;
+
+const ProgressBar = styled.div``;
 
 const SelectContainer = styled.div`
   gap: 8px;
